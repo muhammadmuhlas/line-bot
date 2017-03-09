f5a8a139bf7b1c07a1bcc832a58546eb3e4fa915
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2017-03-09 18:26:58 +0700

    botreplytext

--- htdocs/response.php
+++ htdocs/response.php
@@ -489,7 +489,7 @@ class Response {
 
 					$data = $this->botReceiveSticker($event);
 
-					$this->botSendSticker($event, 630, 4);
+					$this->botSendSticker($event, 4, 630);
 
 				}
 
@@ -510,7 +510,7 @@ class Response {
 
 					$data = $this->botReceiveSticker($event);
 
-					$this->botSendSticker($event, 630, 4);
+					$this->botSendSticker($event, 4, 630);
 
 				}
 
